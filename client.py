af85e74c4e77f0bf9d4fa77487b2cc430ee6b753
rajdev9101 <devraj93957@gmail.com>
2025-01-03 23:45:46 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @Rajdev21_bot
+# Subscribe YouTube Channel For Amazing Bot https://youtub.com
+# Ask Doubt on telegram @raj_dev21
 
 from info import *
 from pyrogram import Client
